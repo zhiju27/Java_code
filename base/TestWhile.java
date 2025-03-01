38f70787ce597457644651f84ee9f93bee14216b
zhiju27 <1622072758@QQ.com>
2025-03-01 08:37:22 +0800

    提交项目文件。

---
+++ base/TestWhile.java
@@ -0,0 +1,22 @@
+package base;
+/**
+ * while型煦暖结构
+ * @author yyy
+ */
+public class TestWhile {
+    public static void main(String[] args) {
+        /*int a = 0;
+        while (a < 3){
+            System.out.println("I love you! ---" + a);
+            a++;
+        }*/
+
+        int i = 0;
+        int sum = 0;
+        while (i <= 100){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
