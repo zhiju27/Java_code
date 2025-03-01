719f9bbbe113812c5c5566f05568a092aa1723c7
zhiju27 <1622072758@QQ.com>
2025-03-01 18:19:28 +0800

    提交项目文件。

--- OOP/src/Person.java
+++ OOP/src/Person.java
@@ -18,6 +18,11 @@ public class Person {
         p2.name = "yyy";
         p2.age = 23;
         p2.show();
+        // Java本质上是值传递，但是如果传递的值是地址的话，那么这个值的属性被修改，则原地址的属性也会被修改
 
+        Person p3 = p1;
+        Person p4 = p1;
+        p3.age = 80;
+        System.out.println("p1.age:" + p1.age + "\tp3.age: " + p3.age + "\tp4.age:" + p4.age);
     }
 }
